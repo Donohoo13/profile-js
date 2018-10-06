293d2fb4d5470aaca88e1ee5f6606778dc52aced
Conner Donohoo <donohoo13@conners-air.lan>
2018-10-05 22:25:12 -0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
-import Navbar from './Navbar1';
+import Navbar from './Navbar';
 
 const App = () => (
   <Router>
